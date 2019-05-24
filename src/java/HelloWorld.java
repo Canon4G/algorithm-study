afff7ed4de02810246c047a0406b0c28ff6f9857
guanhao <guanhao@xinnet.com>
2019-05-24 10:32:38 +0800

    提交

---
+++ src/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package java;
+
+/**
+ * create by Canon4G 2019-05-24
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
